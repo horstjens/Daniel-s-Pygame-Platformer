1cd57ef582d0bdaee146dd4f789cb247e2cbaa48
Daniel-Eichler <L-Drago01@gmx.at>
2014-07-17 11:56:40 +0200

    Update DanielScrollPlatformer.py
    Time based movement, Moving Platform has trouble with player

--- DanielScrollPlatformer.py
+++ DanielScrollPlatformer.py
@@ -23,6 +23,8 @@ BLUE     = (   0,   0, 255)
 RED      = ( 255,   0,   0)
 GREEN    = (   0, 255,   0)
 
+
+
 # Screen dimensions
 SCREEN_WIDTH  = 1800
 SCREEN_HEIGHT = 900
@@ -51,19 +53,23 @@ class Player(pygame.sprite.Sprite):
         # This could also be an image loaded from the disk.
         width = 40
         height = 60
+        self.hitpointsfull=1000.0
+        self.hitpoints=1000.0
         self.image = pygame.Surface([width, height])
         self.image.fill(RED)
+        self.travelplatform = False
 
         # Set a referance to the image rect.
         self.rect = self.image.get_rect()
+        
 
-    def update(self):
+    def update(self, seconds):
         """ Move the player. """
         # Gravity
         self.calc_grav()
 
         # Move left/right
-        self.rect.x += self.change_x
+        self.rect.x += self.change_x*seconds
 
         # See if we hit anything
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
@@ -77,7 +83,7 @@ class Player(pygame.sprite.Sprite):
                 self.rect.left = block.rect.right
 
         # Move up/down
-        self.rect.y += self.change_y
+        self.rect.y += self.change_y*seconds
 
         # Check and see if we hit anything
         block_hit_list = pygame.sprite.spritecollide(self, self.level.platform_list, False)
@@ -86,6 +92,24 @@ class Player(pygame.sprite.Sprite):
             # Reset our position based on the top/bottom of the object.
             if self.change_y > 0:
                 self.rect.bottom = block.rect.top
+                #lava? is the current block where the player stands on made out of lava?
+                if self.level.lava_list.has(block):
+                    self.hitpoints-=2500*seconds # player looses one hitpoint
+                #spikes?  is the current block, where the player stands on made out of spikes?
+                if self.level.spike_list.has(block):
+                    self.hitpoints-=40000*seconds
+                #teleporter? is the current block ,where the playre stands on made out of active teleporters???
+                if self.level.teleporter_list.has(block):
+                    if block.target != None:
+                        #beam player to target teleporter
+                        self.rect.x= block.target.rect.x
+                        self.rect.y =block.target.rect.y-120  # above the target teleporter
+                if self.level.moving_list.has(block):
+                    self.rect.centerx = block.rect.centerx
+                        
+               
+              
+                    
             elif self.change_y < 0:
                 self.rect.top = block.rect.bottom
 
@@ -100,7 +124,7 @@ class Player(pygame.sprite.Sprite):
         if self.change_y == 0:
             self.change_y = 1
         else:
-            self.change_y += .35
+            self.change_y += 25.35
 
         # See if we are on the ground.
         if self.rect.y >= SCREEN_HEIGHT - self.rect.height and self.change_y >= 0:
@@ -119,20 +143,53 @@ class Player(pygame.sprite.Sprite):
 
         # If it is ok to jump, set our speed upwards
         if len(platform_hit_list) > 0 or self.rect.bottom >= SCREEN_HEIGHT:
-            self.change_y = -10
+            self.change_y = -1500
 
     # Player-controlled movement:
     def go_left(self):
         """ Called when the user hits the left arrow. """
-        self.change_x = -6
+        self.change_x = -1400
 
     def go_right(self):
         """ Called when the user hits the right arrow. """
-        self.change_x = 6
+        self.change_x = 1400
 
     def stop(self):
         """ Called when the user lets off the keyboard. """
-        self.change_x = 0
+        if not self.travelplatform:
+           self.change_x = 0
+        
+        
+class Lifebar(pygame.sprite.Sprite):
+    """shows a bar with the hitpoints of player sprite
+    """
+    def __init__(self, boss):
+            #self.groups = allgroup
+            self.boss = boss
+            #self._layer = self.boss._layer
+            pygame.sprite.Sprite.__init__(self)
+            self.oldpercent = 0
+            self.paint()
+            
+            
+    def paint(self):
+            self.image = pygame.Surface((self.boss.rect.width,7))
+            self.image.set_colorkey((0,0,0)) # black transparent
+            pygame.draw.rect(self.image, (0,255,0), (0,0,self.boss.rect.width,7),1)
+            self.rect = self.image.get_rect()
+            
+    def update(self, time):
+            self.percent = self.boss.hitpoints / self.boss.hitpointsfull * 1.0
+            if self.percent != self.oldpercent:
+                self.paint() # important ! boss.rect.width may have changed (because rotating)
+                pygame.draw.rect(self.image, (0,0,0), (1,1,self.boss.rect.width-2,5)) # fill black
+                pygame.draw.rect(self.image, (0,255,0), (1,1,
+                                 int(self.boss.rect.width * self.percent),5),0) # fill green
+            self.oldpercent = self.percent
+            self.rect.centerx = self.boss.rect.centerx
+            self.rect.centery = self.boss.rect.centery - self.boss.rect.height /2 - 10
+            if self.boss.hitpoints < 1: #check if boss is still alive
+                self.kill() # kill the hitbar
 
 class Platform(pygame.sprite.Sprite):
     """ Platform the user can jump on """
@@ -145,6 +202,13 @@ class Platform(pygame.sprite.Sprite):
 
         self.image = pygame.Surface([width, height])
         self.image.fill(BLACK)
+        self.change_x = 0
+        self.change_y = 0
+
+        self.boundary_top = 0
+        self.boundary_bottom = SCREEN_HEIGHT
+        self.boundary_left = 0
+        self.boundary_right = 0
 
         self.rect = self.image.get_rect()
         
@@ -183,7 +247,7 @@ class TeleporterPlatform(pygame.sprite.Sprite):
 
         self.rect = self.image.get_rect()
         
-    def update(self):
+    def update(self, seconds):
         """ Paint the platform blue with color cycle. """
         self.image.fill((0,self.green,255))
         self.green+=self.deltagreen
@@ -205,26 +269,22 @@ class LavaPlatform(Platform):
 
         self.rect = self.image.get_rect()
         
-    def update(self):
+    def update(self, seconds):
         """ Paint the platform red. """
         self.image.fill((random.randint(128,255),0,0))
 
 
 class MovingPlatform(Platform):
     """ This is a fancier platform that can actually move. """
-    change_x = 0
-    change_y = 0
-
-    boundary_top = 0
-    boundary_bottom = 0
-    boundary_left = 0
-    boundary_right = 0
+    
+    
+  
 
     player = None
 
     level = None
 
-    def update(self):
+    def update(self,seconds):
         """ Move the platform.
             If the player is in the way, it will shove the player
             out of the way. This does NOT handle what happens if a
@@ -233,44 +293,64 @@ class MovingPlatform(Platform):
             or add code to handle what happens if they don't. """
 
         # Move left/right
-        self.rect.x += self.change_x
+        self.rect.x += self.change_x*seconds
 
         # See if we hit the player
-        hit = pygame.sprite.collide_rect(self, self.player)
-        if hit:
+        #hit = pygame.sprite.collide_rect(self, self.player)
+        #self.player.travelplatform = False
+        #if hit:
+            #self.player.travelplatform = True
             # We did hit the player. Shove the player around and
             # assume he/she won't hit anything else.
 
             # If we are moving right, set our right side
             # to the left side of the item we hit
-            if self.change_x < 0:
-                self.player.rect.right = self.rect.left
-            else:
+        #    if self.change_x < 0:
+                #self.player.rect.right = self.rect.left
+                #self.player.rect.centerx = self.rect.centerx - self.change_x * seconds
+        #        self.player.change_x = self.change_x
+                
+        #    else:
                 # Otherwise if we are moving left, do the opposite.
-                self.player.rect.left = self.rect.right
-
+                #self.player.rect.left = self.rect.right
+                #self.player.rect.centerx = self.rect.centerx + self.cange_x * seconds
+        #        self.player.change_x = self.change_x
+      
+      
         # Move up/down
-        self.rect.y += self.change_y
+        self.rect.y += self.change_y*seconds
 
-        # Check and see if we the player
-        hit = pygame.sprite.collide_rect(self, self.player)
-        if hit:
+        # Check and see if we hit the player
+        #hit = pygame.sprite.collide_rect(self, self.player)
+        #if hit:
             # We did hit the player. Shove the player around and
             # assume he/she won't hit anything else.
 
             # Reset our position based on the top/bottom of the object.
-            if self.change_y < 0:
-                self.player.rect.bottom = self.rect.top
-            else:
-                self.player.rect.top = self.rect.bottom
+        #    if self.change_y < 0:
+        #        self.player.rect.bottom = self.rect.top
+        #    else:
+        #        self.player.rect.top = self.rect.bottom
 
         # Check the boundaries and see if we need to reverse
         # direction.
-        if self.rect.bottom > self.boundary_bottom or self.rect.top < self.boundary_top:
+        if self.rect.bottom > self.boundary_bottom:
+            self.rect.bottom = self.boundary_bottom
             self.change_y *= -1
-
+        
+        if self.rect.top < self.boundary_top:
+            self.rect.top = self.boundary_top
+            self.change_y *= -1
+        # world shift
         cur_pos = self.rect.x - self.level.world_shift
-        if cur_pos < self.boundary_left or cur_pos > self.boundary_right:
+        
+        # left right boundary ?
+        if cur_pos < self.boundary_left:
+            cur_pos = self.boundary_left
+            self.change_x *= -1
+        
+        if cur_pos > self.boundary_right:
+            cur_pos = self.boundary_right
             self.change_x *= -1
 
 class Level(object):
@@ -294,14 +374,19 @@ class Level(object):
         """ Constructor. Pass in a handle to player. Needed for when moving
             platforms collide with the player. """
         self.platform_list = pygame.sprite.Group()
+        self.lava_list = pygame.sprite.Group()
+        self.spike_list = pygame.sprite.Group()
+        self.teleporter_list = pygame.sprite.Group()
         self.enemy_list = pygame.sprite.Group()
+        self.moving_list = pygame.sprite.Group()
         self.player = player
 
     # Update everythign on this level
-    def update(self):
+    def update(self, seconds):
         """ Update everything in this level."""
-        self.platform_list.update()
-        self.enemy_list.update()
+        self.seconds = seconds
+        self.platform_list.update(self.seconds)
+        self.enemy_list.update(self.seconds)
 
     def draw(self, screen):
         """ Draw everything on this level. """
@@ -337,16 +422,16 @@ class Level_01(Level):
         # Call the parent constructor
         Level.__init__(self, player)
 
-        self.level_limit = -1500
+        self.level_limit = -3000
 
         # Array with width, height, x, and y of platform
         level = [[210, 70, 500, 700],
                  [210, 70, 1000,600],
                  [210, 70, 1300, 450],
-                 [210, 70, 1120, 250],
                  [70, 50, 270, 600],
+                 [70, 50, 2100, 500],
                  [240, 50, 100, 480],
-                 ]
+                 ] 
 
 
         # Go through the array above and add platforms
@@ -363,43 +448,48 @@ class Level_01(Level):
         block.rect.y = 800
         block.player = self.player
         self.platform_list.add(block)
+        self.lava_list.add(block)
        
         
         #Now we add the SpikePlatform :D
-        block= SpikePlatform(2730,50)
+        block= SpikePlatform(4330,50)
         block.rect.x = 0
         block.rect.y =SCREEN_HEIGHT-50
         block.player = self.player
         self.platform_list.add(block)
+        self.spike_list.add(block)
         
-        #Now we add the 1st TeleporterPlatform :D
+        #Now we add the 1st active TeleporterPlatform :D
         tele1= TeleporterPlatform(100, 70)
-        tele1.rect.x = 10
+        tele1.rect.x = 50
         tele1.rect.y = 600
         tele1.player = self.player
         self.platform_list.add(tele1)
+        self.teleporter_list.add(tele1)
         
-        #Now we add the 2nd TeleporterPlatform :D
+        #Now we add the 1st passive TeleporterPlatform :D
         tele2= TeleporterPlatform(100, 70)
-        tele2.rect.x = 1200
+        tele2.rect.x = 2200
         tele2.rect.y = 250
         tele2.player = self.player
         self.platform_list.add(tele2)
+        self.teleporter_list.add(tele2)
         
         #Connect Teleporters
         tele1.target=tele2
-        tele2.target=tele1
+        #tele2.target=tele1
 
         # Add a custom moving platform
         block = MovingPlatform(70, 40)
-        block.rect.x = 1350
-        block.rect.y = 300
-        block.boundary_left = 1350
-        block.boundary_right = 1600
-        block.change_x = 1
+        block.rect.x = 1600
+        block.rect.y = 500
+        block.boundary_left = 1000
+        block.boundary_right = 2000
+        block.change_x = 700
         block.player = self.player
         block.level = self
         self.platform_list.add(block)
+        self.moving_list.add(block)
 
 
 # Create platforms for the level
@@ -522,19 +612,25 @@ def main():
 
     active_sprite_list = pygame.sprite.Group()
     player.level = current_level
-
+    
     player.rect.x = 340
     player.rect.y = SCREEN_HEIGHT - player.rect.height-500
     active_sprite_list.add(player)
-
+    lifebar1=Lifebar (player)
+    active_sprite_list.add(lifebar1)
     #Loop until the user clicks the close button.
     done = False
 
     # Used to manage how fast the screen updates
     clock = pygame.time.Clock()
 
+    playtime = 0.00000
+    FPS= 1000
     # -------- Main Program Loop -----------
     while not done:
+        milliseconds = clock.tick(FPS) # milliseconds passed since last frame
+        seconds = milliseconds / 1000.0 # seconds passed since last frame (float)
+        playtime += seconds
         for event in pygame.event.get(): # User did something
             if event.type == pygame.QUIT: # If user clicked close
                 done = True # Flag that we are done so we exit this loop
@@ -554,10 +650,10 @@ def main():
                     player.stop()
 
         # Update the player.
-        active_sprite_list.update()
+        active_sprite_list.update(seconds)
 
         # Update items in the level
-        current_level.update()
+        current_level.update(seconds)
 
         # If the player gets near the right side, shift the world left (-x)
         if player.rect.right >= 500:
@@ -583,7 +679,11 @@ def main():
                 # Out of levels. This just exits the program.
                 # You'll want to do something better.
                 done = True
-
+        
+        #check if player is alive
+        if player.hitpoints<1:
+            done=True
+        
         # ALL CODE TO DRAW SHOULD GO BELOW THIS COMMENT
         current_level.draw(screen)
         active_sprite_list.draw(screen)
@@ -595,6 +695,7 @@ def main():
 
         # Go ahead and update the screen with what we've drawn.
         pygame.display.flip()
+        pygame.display.set_caption("player1.hitpoints {} FPS: {:.2f}".format(player.hitpoints, clock.get_fps()))
 
     # Be IDLE friendly. If you forget this line, the program will 'hang'
     # on exit.
